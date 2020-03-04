37dd47af5d7c0caccc30168d650060bae53eda88
Joseph Flanagan <gamblinflanagan97@gmail.com>
2020-03-04 13:54:17 -0500

    Create tokens.h

---
+++ tokens.h
@@ -0,0 +1,100 @@
+//
+//  tokens.h
+//  JCF_CS_280_3
+//
+//  Created by Joey on 11/7/18.
+//  Copyright © 2018 Joey. All rights reserved.
+//
+
+#ifndef tokens_h
+#define tokens_h
+
+
+#endif /* tokens_h */
+
+/*
+ * projlex.h
+ *
+ * CS280
+ * Spring 2018
+ */
+
+#ifndef TOKENS_H_
+#define TOKENS_H_
+
+#include <string>
+#include <iostream>
+using std::string;
+using std::istream;
+using std::ostream;
+
+enum TokenType {
+    // keywords
+    PRINT,
+    IF,
+    THEN,
+    TRUE,
+    FALSE,
+    
+    // an identifier
+    IDENT,
+    
+    // an integer and string constant
+    ICONST,
+    SCONST,
+    
+    // the operators, parens and semicolon
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    ASSIGN,
+    EQ,
+    NEQ,
+    LT,
+    LEQ,
+    GT,
+    GEQ,
+    LOGICAND,
+    LOGICOR,
+    LPAREN,
+    RPAREN,
+    SC,
+    
+    // any error returns this token
+    ERR,
+    
+    // when completed (EOF), return this token
+    DONE
+};
+
+class Token {
+    TokenType    tt;
+    string        lexeme;
+    int            lnum;
+    
+public:
+    Token() {
+        tt = ERR;
+        lnum = -1;
+    }
+    Token(TokenType tt, string lexeme, int line) {
+        this->tt = tt;
+        this->lexeme = lexeme;
+        this->lnum = line;
+    }
+    
+    bool operator==(const TokenType tt) const { return this->tt == tt; }
+    bool operator!=(const TokenType tt) const { return this->tt != tt; }
+    
+    TokenType    GetTokenType() const { return tt; }
+    string        GetLexeme() const { return lexeme; }
+    int            GetLinenum() const { return lnum; }
+};
+
+extern ostream& operator<<(ostream& out, const Token& tok);
+
+extern Token getNextToken(istream *in, int *linenum);
+
+
+#endif /* TOKENS_H_ */
